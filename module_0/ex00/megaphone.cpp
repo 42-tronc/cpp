45bfbcd7b85c86727524326f0522d62a454182d8
maple <github.register@slmail.me>
2023-12-11 09:14:08 +0100

    🔄 update: use c++ style cast

--- module_0/ex00/megaphone.cpp
+++ module_0/ex00/megaphone.cpp
@@ -5,7 +5,7 @@ int main(int ac, char *av[])
     if (ac > 1) {
         for (int i = 1; i < ac; i++) {
             for (int j = 0; av[i][j]; j++)
-                std::cout << (char)std::toupper(av[i][j]);
+                std::cout << static_cast<char>(std::toupper(av[i][j]));
         }
     }
     else
