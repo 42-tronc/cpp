e817f5e0a6e11c5af38f684a3993d013b1b64d10
maple <github.register@slmail.me>
2023-12-18 13:51:02 +0100

    🎉 add: module 1 ex 5

---
+++ module_1/ex05/Harl.cpp
@@ -0,0 +1,40 @@
+#include "Harl.hpp"
+
+void Harl::complain(std::string level) {
+    std::string levels[4] = {"debug", "info", "warning", "error"};
+    void (Harl::*functions[4])(void) = {
+        &Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+    for (int i = 0; i < 4; i++) {
+        if (level == levels[i]) {
+            (this->*functions[i])();
+            return;
+        }
+    }
+}
+
+void Harl::debug(void) {
+    std::cout << "\e[1;34m[DEBUG]\e[0m Duuuude, I sure loooove mixing all of "
+                 "the slurpee flavors at the 7-11!"
+              << std::endl;
+}
+
+void Harl::info(void) {
+    std::cout
+        << "\e[1;32m[INFO]\e[0m I am so happy I could get a slurpee right "
+           "now!"
+        << std::endl;
+}
+
+void Harl::warning(void) {
+    std::cout << "\e[1;33m[WARNING]\e[0m What?!? There is no more cherry "
+                 "flavored slurpee? I can't believe this!"
+              << std::endl;
+}
+
+void Harl::error(void) {
+    std::cout
+        << "\e[1;31m[ERROR]\e[0m OOOH I AM SO MAD RIGHT NOW! I swear I "
+           "am not leaving this 7-11 until I get my cherry flavored slurpee!"
+        << std::endl;
+}
