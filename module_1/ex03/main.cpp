54d4568fe867160bfe804d81864cbc37b5411b1a
maple <github.register@slmail.me>
2023-12-19 16:45:50 +0100

    🐛 fix: make better tests

--- module_1/ex03/main.cpp
+++ module_1/ex03/main.cpp
@@ -2,21 +2,43 @@
 #include "HumanB.hpp"
 #include "Weapon.hpp"
 
+void printAction(std::string str) {
+    std::cout << "\n\e[1;33m* \e[21m\e[34m" << str << "\e[0m\n\n";
+}
+
 int main() {
+    printAction("Giving a weapon to HumanA");
     {
         Weapon club = Weapon("crude spiked club");
-        HumanA bob("Bob", club);
-        bob.attack();
+
+        HumanA Michael("Michael", club);
+        Michael.attack();
         club.setType("some other type of club");
-        bob.attack();
+        Michael.attack();
     }
+
+    printAction("Giving a weapon to HumanB before attack");
     {
         Weapon club = Weapon("crude spiked club");
+
         HumanB jim("Jim");
         jim.setWeapon(club);
         jim.attack();
         club.setType("some other type of club");
         jim.attack();
+    }
+
+    printAction(
+        "Giving no weapon to HumanB before attack and then giving a weapon");
+    {
+        Weapon club = Weapon("crude spiked club");
+
+        HumanB Dwight("Dwight");
+        Dwight.attack();
+        Dwight.setWeapon(club);
+        Dwight.attack();
+        club.setType("some other type of club");
+        Dwight.attack();
     }
 
     return 0;
