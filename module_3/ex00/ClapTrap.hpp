3ec821abb2ec20850c4ffa41e3ab8f9fba002dc4
maple <github.register@slmail.me>
2024-01-09 16:35:08 +0100

    🎉 add: ex00

---
+++ module_3/ex00/ClapTrap.hpp
@@ -0,0 +1,34 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap {
+public:
+    ClapTrap(std::string name);
+    ClapTrap(const ClapTrap& other);
+    ~ClapTrap();
+
+    ClapTrap& operator=(ClapTrap const& other);
+
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+    bool checkAlive(ClapTrap& player);
+    bool checkEnergy(ClapTrap& player);
+
+    std::string getName() const;
+    unsigned int getDmg() const;
+    unsigned int getHp() const;
+    unsigned int getEnergy() const;
+    void setDmg(unsigned int dmg);
+
+private:
+    std::string name;
+    unsigned int hp;
+    unsigned int energy;
+    unsigned int dmg;
+};
+
+#endif
