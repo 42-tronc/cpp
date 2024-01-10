581a6381718db0bc583a2fb2be18ec618bea11c7
maple <github.register@slmail.me>
2024-01-10 16:45:16 +0100

    🎉 add: FragTrap class implementation with constructor, destructor, copy constructor, assignment operator, and highFivesGuys method

--- module_3/ex02/FragTrap.cpp
+++ module_3/ex02/FragTrap.cpp
@@ -1,1 +1,47 @@
 #include "FragTrap.hpp"
+
+FragTrap::FragTrap(const std::string& name) : ClapTrap(name) {
+    std::cout << "\e[32mFragTrap (\e[3;37m" << this->name
+              << "\e[;32m) constructor called\e[0m" << std::endl;
+    this->hp = 100;
+    this->energy = 100;
+    this->dmg = 30;
+}
+
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other) {
+    std::cout << "\e[33mFragTrap (\e[3;37m" << other.getName()
+              << "\e[;33m) copy constructor called\e[0m" << std::endl;
+    *this = other;
+}
+
+FragTrap::~FragTrap() {
+    std::cout << "\e[31mFragTrap (\e[3;37m" << this->name
+              << "\e[;31m) destructor called\e[0m" << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& other) {
+    this->name = other.name;
+    this->dmg = other.dmg;
+    this->energy = other.energy;
+    this->hp = other.hp;
+    return *this;
+}
+
+void FragTrap::highFivesGuys(void) {
+    if (!checkAlive(*this)) return;
+    std::string answer;
+    std::cout << "\e[3;37m" << this->name << "\e[0m is requesting a "
+              << "\e[1;35mHigh Five 󰹇\e[0m\n"
+              << "Do you accept? (Y/n) ";
+    getline(std::cin, answer);
+    if (answer.compare("N") == 0 || answer.compare("n") == 0) {
+        std::cout << "\e[3;37m" << this->name
+                  << "\e[0m has been \e[1;31mdenied\e[0m a "
+                  << "\e[1;35mHigh Five 󱩂\e[0m\n";
+        std::cout << "\t\e[21;33mHow sad! 󱚝 \e[0m" << std::endl;
+        return;
+    }
+    std::cout << "\e[3;37m" << this->name << "\e[0m has received a "
+              << "\e[1;35mHigh Five 󱥋\e[0m\n";
+    std::cout << "\t\e[21;33mMan he seems so happy! 󱜙 \e[0m" << std::endl;
+}
--- module_3/ex02/FragTrap.hpp
+++ module_3/ex02/FragTrap.hpp
@@ -1,10 +1,18 @@
 #ifndef FRAGTRAP_HPP
 #define FRAGTRAP_HPP
 
-#include "ScavTrap.hpp"
+#include "ClapTrap.hpp"
 
-class FragTrap {
+class FragTrap : public ClapTrap {
 public:
+    FragTrap(const std::string& name);
+    FragTrap(const FragTrap& other);
+    ~FragTrap();
+
+    FragTrap& operator=(const FragTrap& other);
+
+    void highFivesGuys(void);
+
 private:
 };
 
