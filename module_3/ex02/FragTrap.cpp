0dafa392e3bf65230c5a4db4e4289200e43a2f89
maple <github.register@slmail.me>
2024-01-15 18:34:25 +0100

    🔄 update: Add default constructor to FragTrap class

--- module_3/ex02/FragTrap.cpp
+++ module_3/ex02/FragTrap.cpp
@@ -1,5 +1,14 @@
 #include "FragTrap.hpp"
 
+FragTrap::FragTrap() : ClapTrap("FragTrap") {
+    std::cout << "\e[32mFragTrap (\e[3;37m" << this->name
+              << "\e[;32m) default constructor called\e[0m" << std::endl;
+    this->hp = 100;
+    this->maxHp = this->hp;
+    this->energy = 100;
+    this->dmg = 30;
+}
+
 FragTrap::FragTrap(const std::string& name) : ClapTrap(name) {
     std::cout << "\e[32mFragTrap (\e[3;37m" << this->name
               << "\e[;32m) constructor called\e[0m" << std::endl;
