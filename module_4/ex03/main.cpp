45cc1e6050b024a6f9da7bd4833bbcbb27802882
maple <github.register@slmail.me>
2024-01-18 19:10:52 +0100

    🔄 update: change main

--- module_4/ex03/main.cpp
+++ module_4/ex03/main.cpp
@@ -9,109 +9,119 @@ void printAction(const std::string& str) {
 
 int main(void) {
     // printAction("Using the subject's main");
-    // IMateriaSource* src = new MateriaSource();
-    // src->learnMateria(new Ice());
-    // src->learnMateria(new Cure());
-    // ICharacter* me = new Character("me");
-    // AMateria* tmp;
-    // tmp = src->createMateria("ice");
-    // me->equip(tmp);
-    // tmp = src->createMateria("cure");
-    // me->equip(tmp);
-    // ICharacter* bob = new Character("bob");
-    // me->use(0, *bob);
-    // me->use(1, *bob);
-    // delete bob;
-    // delete me;
-    // delete src;
+    // {
+    //     IMateriaSource* src = new MateriaSource();
+    //     src->learnMateria(new Ice());
+    //     src->learnMateria(new Cure());
 
-    // MateriaSource* src = new MateriaSource();
-    // src->learnMateria(new Ice());
-    // src->learnMateria(new Cure());
-    // src->learnMateria(new Ice());
-    // src->learnMateria(new Cure());
-    // src->learnMateria(new Cure());
+    //     std::cout << std::endl;
 
-    // // std::cout << std::endl;
+    //     ICharacter* me = new Character("me");
+    //     AMateria* tmp;
+    //     tmp = src->createMateria("ice");
+    //     me->equip(tmp);
+    //     tmp = src->createMateria("cure");
+    //     me->equip(tmp);
 
-    // Character* me = new Character("me");
-    // AMateria* tmp;
-    // tmp = src->createMateria("ice");
-    // me->equip(tmp);
-    // tmp = src->createMateria("fuyfyufyiu");
-    // me->equip(tmp);
-    // tmp = src->createMateria("ice");
-    // me->equip(tmp);
-    // tmp = src->createMateria("cure");
-    // me->equip(tmp);
-    // tmp = src->createMateria("ice");
-    // me->equip(tmp);
-    // tmp = src->createMateria("cure");
-    // me->equip(tmp);
+    //     std::cout << std::endl;
 
-    // // std::cout << std::endl;
+    //     ICharacter* bob = new Character("bob");
+    //     me->use(0, *bob);
+    //     me->use(1, *bob);
 
-    // ICharacter* bob = new Character("bob");
-    // me->unequip(0);
-    // me->unequip(0);
-    // me->unequip(5);
-    // for (int i = -1; i < 4; i++) me->use(i, *bob);
-    // tmp = src->createMateria("cure");
-    // for (int i = 0; i < 2; i++) me->equip(tmp);
-    // me->use(0, *bob);
+    //     delete bob;
+    //     delete me;
+    //     delete src;
+    // }
 
-    // delete bob;
-    // delete me;
-    // delete src;
+    printAction("modzie");
+    MateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    src->learnMateria(new Cure());
 
-    printAction("Creating a MateriaSource");
-    IMateriaSource* src = new MateriaSource();
+    std::cout << std::endl;
 
-    printAction("Creating a Character");
-    ICharacter* me = new Character("John");
-
-    printAction("Equipping the Character with an Ice Materia");
-    AMateria* ice = new Ice();
-    src->learnMateria(ice);
-    me->equip(ice);
-
-    printAction("Equipping the Character with a Cure Materia");
-    AMateria* cure = new Cure();
-    src->learnMateria(cure);
-    me->equip(cure);
-
-    printAction("Creating another Character");
-    ICharacter* bob = new Character("Bob");
-
-    printAction("Using the Ice Materia on Bob");
-    me->use(0, *bob);
-
-    printAction("Using the Cure Materia on Bob");
-    me->use(1, *bob);
-
-    printAction("Unequipping the already used Ice Materia");
-    me->unequip(0);
-
-    printAction("Creating multiple Materia");
+    Character* me = new Character("me");
     AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("fuyfyufyiu");
+    me->equip(tmp);
     tmp = src->createMateria("ice");
     me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
-    tmp = src->createMateria("fire");
-    me->equip(tmp);
-    tmp = src->createMateria("water");
+    tmp = src->createMateria("ice");
     me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
 
-    printAction("Using the Ice Materia on Bob");
+    std::cout << std::endl;
+
+    ICharacter* bob = new Character("bob");
+    me->unequip(0);
+    me->unequip(0);
+    me->unequip(5);
+    for (int i = -1; i < 4; i++) me->use(i, *bob);
+    tmp = src->createMateria("cure");
+    for (int i = 0; i < 5; i++) me->equip(tmp);
+    me->use(0, *bob);
+
+    delete bob;
+    delete me;
+    delete src;
+
+    // printAction("Creating a MateriaSource");
+    // IMateriaSource* src = new MateriaSource();
+
+    // printAction("Creating a Character");
+    // ICharacter* me = new Character("John");
+
+    // printAction("Equipping the Character with an Ice Materia");
+    // AMateria* ice = new Ice();
+    // src->learnMateria(ice);
+    // me->equip(ice);
+
+    // printAction("Equipping the Character with a Cure Materia");
+    // AMateria* cure = new Cure();
+    // src->learnMateria(cure);
+    // me->equip(cure);
+
+    // printAction("Creating another Character");
+    // ICharacter* bob = new Character("Bob");
+
+    // printAction("Using the Ice Materia on Bob");
     // me->use(0, *bob);
 
-    printAction("Exiting the program");
-    delete me;
-    delete bob;
-    delete src;
+    // printAction("Using the Cure Materia on Bob");
+    // me->use(1, *bob);
+
+    // printAction("Unequipping the already used Ice Materia");
+    // me->unequip(0);
+
+    // printAction("Creating multiple Materia");
+    // AMateria* tmp;
+    // tmp = src->createMateria("ice");
+    // me->equip(tmp);
+    // tmp = src->createMateria("cure");
+    // me->equip(tmp);
+    // tmp = src->createMateria("fire");
+    // me->equip(tmp);
+    // tmp = src->createMateria("water");
+    // me->equip(tmp);
+    // tmp = src->createMateria("cure");
+    // me->equip(tmp);
+
+    // printAction("Using the Ice Materia on Bob");
+    // // me->use(0, *bob);
+
+    // printAction("Exiting the program");
+    // // delete bob;
+    // delete me;
+    // delete src;
 
     return 0;
 }
