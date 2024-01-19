370964aae3b539b790970b0418df19a41751243e
maple <github.register@slmail.me>
2024-01-19 13:37:11 +0100

    🔄 update: Brain class print statements with a cool looking emoji

--- module_4/ex01/Brain.cpp
+++ module_4/ex01/Brain.cpp
@@ -4,20 +4,20 @@
 #include <string>
 
 Brain::Brain() {
-    std::cout << "\e[32mBrain\e[0m constructor called\n";
+    std::cout << "\e[32m󰧑 Brain\e[0m constructor called\n";
 }
 
 Brain::Brain(const Brain& other) {
-    std::cout << "\e[33mBrain\e[0m copy constructor called\n";
+    std::cout << "\e[33m󰧑 Brain\e[0m copy constructor called\n";
     *this = other;
 }
 
 Brain::~Brain() {
-    std::cout << "\e[31mBrain\e[0m destructor called\n";
+    std::cout << "\e[31m󰧑 Brain\e[0m destructor called\n";
 }
 
 Brain& Brain::operator=(const Brain& other) {
-    std::cout << "\e[33mBrain\e[0m assignation operator called\n";
+    std::cout << "\e[33m󰧑 Brain\e[0m assignation operator called\n";
     for (int i = 0; i < 100; i++) ideas[i] = other.ideas[i];
     return *this;
 }
