afd4a1ad45e70783338ed8648cdc7df2993cb6b6
maple <github.register@slmail.me>
2024-03-10 15:13:02 +0100

    🎉 add: 00

--- module_5/ex00/Bureaucrat.cpp
+++ module_5/ex00/Bureaucrat.cpp
@@ -1,1 +1,15 @@
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : name("default"), grade(150) {
+    std::cout << "\e[32mBureaucrat " << name << " was created\e[0m"
+              << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string name, short grade) : name(name) {
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException(
+            "Grade cannot be higher than 1");
+    else if (grade > 150)
+        throw Bureaucrat::GradeTooLowException(
+            "Grade cannot be lower than 150");
+}
--- module_5/ex00/Bureaucrat.hpp
+++ module_5/ex00/Bureaucrat.hpp
@@ -2,20 +2,22 @@
 #define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <stdexcept>
 
 class Bureaucrat {
 public:
     Bureaucrat();
-    Bureaucrat(const std::string name);
+    Bureaucrat(const std::string name, short grade);
     Bureaucrat(const Bureaucrat &other);
     ~Bureaucrat();
 
     Bureaucrat &operator=(const Bureaucrat &other);
 
+    const std::string &getName() const;
+
 private:
     const std::string name;
-
-protected:
+    short grade;
 };
 
 #endif
