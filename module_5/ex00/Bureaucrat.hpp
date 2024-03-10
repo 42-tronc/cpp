ec0dc4bce63407cac1b44c9fdc77f169b937e617
maple <github.register@slmail.me>
2024-03-10 16:56:44 +0100

    🎉 add: working ex00

--- module_5/ex00/Bureaucrat.hpp
+++ module_5/ex00/Bureaucrat.hpp
@@ -14,10 +14,33 @@ public:
     Bureaucrat &operator=(const Bureaucrat &other);
 
     const std::string &getName() const;
+    const short &getGrade() const;
+    void setGrade(short grade);
+
+    void incrementGrade();
+    void decrementGrade();
+
+    class GradeTooHighException : public std::exception {
+    public:
+        const char *what() const
+            throw() {  // throw() is only for c++98 (tells the compiler that the
+                       // function will not throw any exceptions)
+            return "Grade cannot be higher than 1";
+        }
+    };
+
+    class GradeTooLowException : public std::exception {
+    public:
+        const char *what() const throw() {
+            return "Grade cannot be lower than 150";
+        }
+    };
 
 private:
     const std::string name;
     short grade;
 };
+
+std::ostream &operator<<(std::ostream &stream, const Bureaucrat &bureaucrat);
 
 #endif
--- module_5/ex00/main.cpp
+++ module_5/ex00/main.cpp
@@ -1,5 +1,44 @@
 #include "Bureaucrat.hpp"
 
+void printAction(const std::string& str) {
+    std::cout << "\n\e[1;33m* \e[21m\e[34m" << str << "\e[0m\n\n";
+}
+
 int main() {
+    printAction("Creating a Bureaucrat with a grade too high");
+    Bureaucrat tooHigh("TooHigh", 0);
+
+    printAction("Creating a Bureaucrat with a grade too low");
+    Bureaucrat tooLow("TooLow", 151);
+
+    printAction("Creating a Bureaucrat with a valid grade");
+    Bureaucrat haulande("Francois Haulande", 100);
+
+    printAction("Increment the grade of a Bureaucrat");
+    haulande.incrementGrade();
+
+    printAction("Creating a new Bureaucrat and decrementing its grade");
+    Bureaucrat macaron("Emmanuel Macaron", 149);
+    macaron.decrementGrade();
+
+    printAction("Decreasing the grade of a Bureaucrat with a grade too low");
+    macaron.decrementGrade();
+
+    printAction("Creating a copy of a Bureaucrat");
+    Bureaucrat copy(macaron);
+
+    printAction("Creating a copy with the assignment operator");
+    Bureaucrat copy2 = haulande;
+
+    printAction("Using the operator<< to print all the Bureaucrats");
+    std::cout << tooHigh << "\n"
+              << tooLow << "\n"
+              << haulande << "\n"
+              << macaron << "\n"
+              << copy << "\n"
+              << copy2 << std::endl;
+
+    printAction("Exiting the program");
+
     return 0;
 }
