b935d05c9b18a704bdacce35ea284cb463882d5d
maple <github.register@slmail.me>
2024-03-20 10:53:57 +0100

    🎉 add: my mainnnnn

--- module_5/ex03/main.cpp
+++ module_5/ex03/main.cpp
@@ -1,160 +1,64 @@
 #include "Intern.hpp"
 
-void printAction(const std::string &str) {
+void printAction(const std::string& str) {
     std::cout << "\n\e[1;33m* \e[21m\e[34m" << str << "\e[0m\n\n";
 }
 
 int main() {
-    // printAction("Create an Intern");
-    // Intern intern;
-    // AForm* shrub;
-    // AForm* robot;
-    // AForm* pres;
+    printAction("Create an Intern");
+    Intern intern;
 
-    // printAction("Create a Shrubbery Creation AForm");
-    // shrub = intern.makeForm("shrubbery creation", "home");
+    printAction("Create a Shrubbery Creation AForm");
+    AForm* shrub = intern.makeForm("shrubbery creation", "home");
 
-    // printAction("Create a Robotomy Request AForm");
-    // robot = intern.makeForm("robotomy request", "home");
+    printAction("Create a Robotomy Request AForm");
+    AForm* robot = intern.makeForm("robotomy request", "home");
 
-    // printAction("Create a Presidential Pardon AForm");
-    // pres = intern.makeForm("presidential pardon", "home");
+    printAction("Create a Presidential Pardon AForm");
+    AForm* pres = intern.makeForm("presidential pardon", "home");
 
-    // printAction("Delete the forms");
-    // delete shrub;
-    // delete robot;
-    // delete pres;
+    printAction("Print the form's stats");
+    std::cout << *shrub << std::endl
+              << *robot << std::endl
+              << *pres << std::endl;
 
-    /* std::cout << std::endl
-              << std::endl
-              << "\033[1m\033[32m"
-              << "TESTS EX03 - Testing Intern"
-              << "\033[0m" << std::endl;
-    std::string name = "invalid name";
-    printAction("Creating intern");
-    Intern someRandomIntern;
+    printAction("Create a Bureaucrat with a High Grade");
+    Bureaucrat high("High", 1);
 
-    printAction("Using intern to create AForm, invalid name");
-    AForm* invalidForm = someRandomIntern.makeForm("invalid name", "Target");
+    printAction("Create a Bureaucrat with a Low Grade");
+    Bureaucrat low("Low", 150);
 
-    printAction("Displaying form stats");
-    std::cout << invalidForm << std::endl;
+    printAction("Create a Bureaucrat with a Normal Grade");
+    Bureaucrat normal("Normal", 75);
 
-    printAction("Using intern to create AForm, Shrubbery");
-    AForm* shrubberyForm =
-        someRandomIntern.makeForm("shrubbery creation", "Target");
+    printAction("Make the Low Grade Bureaucrat try to sign the forms");
+    shrub->beSigned(low);
+    robot->beSigned(low);
+    pres->beSigned(low);
 
-    printAction("Displaying Shrubbery stats");
-    std::cout << *shrubberyForm << std::endl;
+    printAction("Make the Low Grade Bureaucrat try to execute the forms");
+    shrub->execute(low);
+    robot->execute(low);
+    pres->execute(low);
 
-    printAction("Using intern to create AForm, Robotomy");
-    AForm* robotomyForm =
-        someRandomIntern.makeForm("robotomy request", "Target");
+    printAction("Make the High Grade Bureaucrat try to sign the forms");
+    shrub->beSigned(high);
+    robot->beSigned(high);
+    pres->beSigned(high);
 
-    printAction("Displaying Robotomy stats");
-    std::cout << *robotomyForm << std::endl;
+    printAction("High Grade executes Shrubbery Creation");
+    shrub->execute(high);
 
-    printAction("Using intern to create AForm, Presidential");
-    AForm* presidentialForm =
-        someRandomIntern.makeForm("presidential pardon", "Target");
+    printAction("High Grade executes Robotomy Request");
+    robot->execute(high);
 
-    printAction("Displaying Presidential stats");
-    std::cout << *presidentialForm << std::endl;
+    printAction("High Grade executes Presidential Pardon");
+    pres->execute(high);
 
-    printAction("Creating Bureaucrat");
-    Bureaucrat memberLow("MemberLow", 150);
-    Bureaucrat memberHigh("MemberHigh", 1);
-
-    printAction("memberLow trying to sign all form");
-    memberLow.executeAForm(*shrubberyForm);
-    memberLow.executeAForm(*robotomyForm);
-    memberLow.executeAForm(*presidentialForm);
-
-    printAction("memberHigh signing all form");
-    memberHigh.signAForm(*shrubberyForm);
-    memberHigh.signAForm(*robotomyForm);
-    memberHigh.signAForm(*presidentialForm);
-
-    printAction("memberLow trying to sign again");
-    memberLow.executeAForm(*shrubberyForm);
-    memberLow.executeAForm(*robotomyForm);
-    memberLow.executeAForm(*presidentialForm);
-
-    printAction("memberHigh execute shrubberyForm");
-    memberHigh.executeAForm(*shrubberyForm);
-
-    printAction("memberHigh execute robotomyForm");
-    memberHigh.executeAForm(*robotomyForm);
-
-    printAction("memberHigh execute presidentialForm");
-    memberHigh.executeAForm(*presidentialForm);
-
-    printAction("Deleting all");
-    delete invalidForm;
-    delete shrubberyForm;
-    delete robotomyForm;
-    delete presidentialForm */
-    ;
-
-    Intern slave;
-    AForm *scf;
-    AForm *rrf;
-    AForm *ppf;
-
-    try {
-        scf = slave.makeForm("shrubbery creation", "Forest");
-        rrf = slave.makeForm("robotomy request", "Bender");
-        ppf = slave.makeForm("presidential pardon", "PresPard");
-        // AForm *wrong;
-        // wrong = slave.makeForm("Shrubbery creation", "Dedge");
-    } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-        delete scf;
-        delete rrf;
-        delete ppf;
-        return 1;
-    }
-
-    {
-        printAction("RobotoMyRequestForm");
-        Bureaucrat thomas("Thomas", 5);
-        thomas.signAForm(*scf);
-        thomas.executeAForm(*scf);
-        Bureaucrat mehdi("Mehdi", 145);
-        mehdi.signAForm(*scf);
-        mehdi.executeAForm(*scf);
-        Bureaucrat clement("Clement", 150);
-        clement.signAForm(*scf);
-        clement.executeAForm(*scf);
-    }
-    {
-        printAction("ShrubberyCreationForm");
-        Bureaucrat thomas("Thomas", 5);
-        thomas.signAForm(*rrf);
-        thomas.executeAForm(*rrf);
-        Bureaucrat mehdi("Mehdi", 72);
-        mehdi.signAForm(*rrf);
-        mehdi.executeAForm(*rrf);
-        Bureaucrat clement("Clement", 150);
-        clement.signAForm(*rrf);
-        clement.executeAForm(*rrf);
-    }
-    {
-        printAction("PresidentialPardonForm");
-        Bureaucrat thomas("Thomas", 5);
-        thomas.signAForm(*ppf);
-        thomas.executeAForm(*ppf);
-        Bureaucrat mehdi("Mehdi", 25);
-        mehdi.signAForm(*ppf);
-        mehdi.executeAForm(*ppf);
-        Bureaucrat clement("Clement", 150);
-        clement.signAForm(*ppf);
-        clement.executeAForm(*ppf);
-    }
-
-    delete scf;
-    delete rrf;
-    delete ppf;
+    printAction("Delete the forms");
+    delete shrub;
+    delete robot;
+    delete pres;
 
     printAction("Exiting the program");
 
