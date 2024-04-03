1a13d35ee3c358797a1b93c2335e6c3939684c4f
maple <github.register@slmail.me>
2024-04-03 11:38:48 +0200

    🎉 add: m07 ex00 wip

--- module_7/ex00/main.cpp
+++ module_7/ex00/main.cpp
@@ -1,11 +1,20 @@
+#include <iomanip>
+#include <iostream>
 
+#include "whatever.hpp"
 
 void printAction(const std::string& str) {
     std::cout << "\n\e[1;33m* \e[21m\e[34m" << str << "\e[0m\n\n";
 }
 
 int main(void) {
-    printAction("hej");
+    printAction("Define two variables and swap them");
+    int a = 2;
+    int b = 3;
+    std::cout << "a = " << a << std::setw(8) << "b = " << b << std::endl;
+
+    swap(a, b);
+    std::cout << "a = " << a << std::setw(8) << "b = " << b << std::endl;
 
     return 0;
 }
--- module_7/ex00/whatever.hpp
+++ module_7/ex00/whatever.hpp
@@ -1,4 +1,11 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+template <typename T, typename U>
+void swap(T &a, U &b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
 #endif
