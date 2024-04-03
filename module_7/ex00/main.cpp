94b34683c027a824a9c06e5a7d6a6d33144926f8
maple <github.register@slmail.me>
2024-04-03 13:35:22 +0200

    🎉 add: m07 ex00 done

--- module_7/ex00/main.cpp
+++ module_7/ex00/main.cpp
@@ -13,8 +13,42 @@ int main(void) {
     int b = 3;
     std::cout << "a = " << a << std::setw(8) << "b = " << b << std::endl;
 
-    swap(a, b);
+    ::swap(a, b);
     std::cout << "a = " << a << std::setw(8) << "b = " << b << std::endl;
+
+    printAction("Use min function to find the smallest value");
+    std::cout << "min(" << a << ", " << b << ") = " << ::min(a, b) << std::endl;
+
+    printAction("Use max function to find the biggest value");
+    std::cout << "max(" << a << ", " << b << ") = " << ::max(a, b) << std::endl;
+
+    printAction("Define two double variables and swap them");
+    double c = 2.5;
+    double d = 9.5;
+    std::cout << "c = " << c << std::setw(8) << "d = " << d << std::endl;
+
+    ::swap(c, d);
+    std::cout << "c = " << c << std::setw(8) << "d = " << d << std::endl;
+
+    printAction("Use min function to find the smallest value");
+    std::cout << "min(" << c << ", " << d << ") = " << ::min(c, d) << std::endl;
+
+    printAction("Use max function to find the biggest value");
+    std::cout << "max(" << c << ", " << d << ") = " << ::max(c, d) << std::endl;
+
+    printAction("Define two strings and swap them");
+    std::string e = "chaine1";
+    std::string f = "chaine2";
+    std::cout << "e = " << e << std::setw(8) << "f = " << f << std::endl;
+
+    ::swap(e, f);
+    std::cout << "e = " << e << std::setw(8) << "f = " << f << std::endl;
+
+    printAction("Use min function to find the smallest value");
+    std::cout << "min(" << e << ", " << f << ") = " << ::min(e, f) << std::endl;
+
+    printAction("Use max function to find the biggest value");
+    std::cout << "max(" << e << ", " << f << ") = " << ::max(e, f) << std::endl;
 
     return 0;
 }
--- module_7/ex00/whatever.hpp
+++ module_7/ex00/whatever.hpp
@@ -1,11 +1,21 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-template <typename T, typename U>
-void swap(T &a, U &b) {
+template <typename T>
+void swap(T &a, T &b) {
     T tmp = a;
     a = b;
     b = tmp;
+}
+
+template <typename T>
+T min(T first, T second) {
+    return first > second ? second : first;
+}
+
+template <typename T>
+T max(T first, T second) {
+    return first > second ? first : second;
 }
 
 #endif
