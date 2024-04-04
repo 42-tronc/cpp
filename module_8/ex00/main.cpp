353937e739142bcae27e066cae416035c9ec2a09
maple <github.register@slmail.me>
2024-04-04 16:49:48 +0200

    🎉 add: m08 ex00 wip

--- module_8/ex00/main.cpp
+++ module_8/ex00/main.cpp
@@ -1,10 +1,12 @@
 
+#include <iostream>
+
 void printAction(const std::string& str) {
     std::cout << "\n\e[1;33m* \e[21m\e[34m" << str << "\e[0m\n" << std::endl;
 }
 
 int main(void) {
-    printAction("hey");
+    printAction("ZIZI");
 
     return 0;
 }
