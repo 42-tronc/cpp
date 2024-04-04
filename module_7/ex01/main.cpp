bdc36c453dcfba0767e29c8063557b4b669986f2
maple <github.register@slmail.me>
2024-04-04 13:12:35 +0200

    🐛 fix: to work with const

--- module_7/ex01/main.cpp
+++ module_7/ex01/main.cpp
@@ -2,24 +2,35 @@
 
 #include "iter.hpp"
 
+void printAction(const std::string& str) {
+    std::cout << "\n\e[1;33m* \e[21m\e[34m" << str << "\e[0m\n\n";
+}
+
 template <typename T>
-void print(T &elem) {
+void print(const T& elem) {
     std::cout << elem << std::endl;
 }
 
 int main(void) {
+    printAction("Creating arrays");
     int intArray[] = {1, 2, 3, 4, 5};
     char charArray[] = {'a', 'b', 'c', 'd', 'e'};
     std::string strArray[] = {"one", "two", "three", "four", "five"};
 
-    std::cout << "Printing intArray:" << std::endl;
+    printAction("Printing arrays");
     iter(intArray, 5, print);
 
-    std::cout << "\nPrinting charArray:" << std::endl;
+    printAction("Printing charArray");
     iter(charArray, 5, print);
 
-    std::cout << "\nPrinting strArray:" << std::endl;
+    printAction("Printing strArray");
     iter(strArray, 5, print);
+
+    printAction("Create const arrays");
+    const int intArrayConst[] = {1, 2, 3, 4, 5};
+
+    std::cout << "Printing intArrayConst:" << std::endl;
+    iter(intArrayConst, 5, print);
 
     return 0;
 }
