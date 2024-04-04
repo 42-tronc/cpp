79fd532805357225c2745d68e552740326ea0a5f
maple <github.register@slmail.me>
2024-04-04 15:11:49 +0200

    🎉 add: base

---
+++ module_8/ex01/main.cpp
@@ -0,0 +1,10 @@
+
+void printAction(const std::string& str) {
+    std::cout << "\n\e[1;33m* \e[21m\e[34m" << str << "\e[0m\n" << std::endl;
+}
+
+int main(void) {
+    printAction("hey");
+
+    return 0;
+}
