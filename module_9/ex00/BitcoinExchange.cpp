85c3473ee8f5477ec88dac13f998ebd74246b3aa
maple <github.register@slmail.me>
2024-04-18 18:52:46 +0200

    🔄 update:  remove unnecessary code in BitcoinExchange constructor

--- module_9/ex00/BitcoinExchange.cpp
+++ module_9/ex00/BitcoinExchange.cpp
@@ -1,20 +1,11 @@
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange(const std::string& filename) {
-    std::cout << "\e[32m󰠓 Class created\e[;m" << std::endl;
-    (void)filename;
-    // checkFileContent("data.csv", true);
-    // checkFileContent(filename);
+    checkFileContent("data.csv", true);
+    checkFileContent(filename);
 }
 
-BitcoinExchange::~BitcoinExchange() {
-    std::cout << "\e[31m󰠓 Class destroyed\e[;m" << std::endl;
-}
-
-bool BitcoinExchange::isFileMissing(const std::string& filename) {
-    std::ifstream file(filename.c_str());
-    return !file.good();
-}
+BitcoinExchange::~BitcoinExchange() {}
 
 void BitcoinExchange::addEntry(const std::string& date, float exchangeRate) {
     this->data[date] = exchangeRate;
