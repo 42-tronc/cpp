01b7d6317988cd714a7be493fecdbbe6c58511cf
maple <github.register@slmail.me>
2024-04-23 17:09:53 +0200

    🎉 add: copy constructor and assignment operator

--- module_9/ex00/BitcoinExchange.cpp
+++ module_9/ex00/BitcoinExchange.cpp
@@ -5,7 +5,18 @@ BitcoinExchange::BitcoinExchange(const std::string& filename) {
     checkFileContent(filename);
 }
 
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) {
+    this->data = other.data;
+}
+
 BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
+    if (this != &other)
+        this->data = other.data;
+
+    return *this;
+}
 
 void BitcoinExchange::addEntry(const std::string& date, float exchangeRate) {
     this->data[date] = exchangeRate;
