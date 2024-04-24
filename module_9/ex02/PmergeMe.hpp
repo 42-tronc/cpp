e3375c8e6e539b93ecb91feb2add02d5cbd9db17
maple <github.register@slmail.me>
2024-04-24 16:35:53 +0200

    🎉 add: PmergeMe class

--- module_9/ex02/PmergeMe.hpp
+++ module_9/ex02/PmergeMe.hpp
@@ -1,7 +1,9 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+#include <deque>
 #include <iostream>
+#include <vector>
 
 inline void printError(const std::string &message) {
     std::string colour, prefix;
@@ -13,5 +15,20 @@ inline void printError(const std::string &message) {
     std::cout << "\n\e[1;33mUsage: \e[;33m./PmergeMe [positive int] [positive "
               << "int] ...\e[0m" << std::endl;
 }
+
+class PmergeMe {
+public:
+    PmergeMe();
+    PmergeMe(const PmergeMe &other);
+    ~PmergeMe();
+
+    PmergeMe &operator=(const PmergeMe &other);
+
+private:
+    std::vector<int> vector;
+    std::deque<int> deque;
+    double execTimeVector;
+    double execTimeDeque;
+};
 
 #endif
